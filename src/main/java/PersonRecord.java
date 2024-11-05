c9d308ba95f5f1f7dad91dbca2db5f47c38084f9
frevib <hdevries@fastmail.com>
2024-11-05 09:06:45 +0100

    Add Java record

---
+++ src/main/java/PersonRecord.java
@@ -0,0 +1,5 @@
+public record PersonRecord(
+        String name,
+        Integer age
+) {
+}
--- src/main/kotlin/DataClass.kt
+++ src/main/kotlin/DataClass.kt
@@ -4,6 +4,9 @@ class Person(val name: String, val age: Int)
 
 data class PersonData(val name: String, val age: Int)
 
+@JvmRecord
+data class PersonData2(val name: String, val age: Int)
+
 val person = PersonData(
     name = "Alice",
     age = 29
